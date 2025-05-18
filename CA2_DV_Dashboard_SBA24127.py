281db6346ce8d03d43227d7ed872e0661dc58fe4
ND272-NTD <sba24127@student.cct.ie>
2025-05-18 22:30:00 +0100

    Update CA2_DV_Dashboard_SBA24127.py

--- CA2_DV_Dashboard_SBA24127.py
+++ CA2_DV_Dashboard_SBA24127.py
@@ -15,7 +15,7 @@ alt.themes.enable("dark")
 
 df = pd.read_csv('movies_streamlit.csv')
 
-ratings_df = pd.read_csv("rating.csv, encoding='ISO-8859-1')
+ratings_df = pd.read_csv('rating.csv',encoding='ISO-8859-1')
 
 #st.title('Movies 4 U 🍿🍿🍿')
 
@@ -61,7 +61,7 @@ col = st.columns((1.5, 4.5, 2), gap='medium')
 
 with col[0]:
     st.markdown('Movies 4 U stats')
-    st.metric("Total Movies to watch", ratings_count)
+    st.metric("Total Movies to watch", movie_count)
     
     st.markdown('Total User Ratings')
     st.metric("Total Ratings count", ratings_count)
